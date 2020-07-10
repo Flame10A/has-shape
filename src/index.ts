bfe4217316b43f08ef553cfd06fc4beaea5b3016
Ollie Bell <flame.10a@googlemail.com>
2020-07-10 08:08:31 +0100

    Fix index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default } from './hasShape';
 export { default as arrayShape } from './arrayShape';
+export { default as assertShape } from './assertShape';
 export { default as compileShape, compileShapeAssertion } from './compileShape';
 export * from './shapeTypes';
