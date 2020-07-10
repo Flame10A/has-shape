efcde2388feb56b7b6b063420d921d38c930699f
Ollie Bell <flame.10a@googlemail.com>
2020-07-10 08:08:43 +0100

    Remove old tests

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,21 +1,4 @@
-var isClass = require('../dist/utils').isClass;
 var isValidInput = require('../dist/utils').isValidInput;
-var safeStringify = require('../dist/utils').safeStringify;
-
-describe('isClass() util', () => {
-    function DummyClass() {
-        this.n = 0;
-    }
-
-    it('Returns true when passed a class/constructor function', () => {
-
-        expect(isClass(DummyClass)).toBe(true);
-        expect(isClass(() => ({ n: 0 }))).toBe(false);
-        expect(isClass({})).toBe(false);
-        expect(isClass(null)).toBe(false);
-    });
-
-});
 
 describe('isValidInput() util', () => {
 
@@ -31,23 +14,6 @@ describe('isValidInput() util', () => {
         expect(isValidInput('hello!')).toBe(false);
         expect(isValidInput(null)).toBe(false);
         expect(isValidInput(undefined)).toBe(false);
-    });
-
-});
-
-describe('safeStringify() util', () => {
-
-    it('Returns the same output as JSON.stringify() for a safe value', () => {
-        var obj = { x: { y: 'z' } };
-
-        expect(safeStringify(obj)).toBe(JSON.stringify(obj, undefined, 1));
-    });
-
-    it('Returns a string even if there\'s a circular reference', () => {
-        var obj = { x: null };
-        obj.x = obj;
-
-        expect(typeof safeStringify(obj)).toBe('string');
     });
 
 });
